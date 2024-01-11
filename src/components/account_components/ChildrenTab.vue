551c6cb0d6348ac0a51cd8cf8afaeef7de992c80
ivanLFN <LFN.current@yandex.com>
2024-01-11 15:53:24 -0800

    update: adding form for edit child information

--- src/components/account_components/ChildrenTab.vue
+++ src/components/account_components/ChildrenTab.vue
@@ -116,6 +116,116 @@
       </div>
     </div>
   </div>
+  <div
+    id="staticBackdropcEdit"
+    class="modal fade"
+    data-bs-backdrop="static"
+    data-bs-keyboard="false"
+    tabindex="-1"
+    aria-labelledby="staticBackdropLabel"
+    aria-hidden="true"
+  >
+    <div class="modal-dialog modal-dialog-centered custom-modal">
+      <div class="modal-content">
+        <div class="modal-body">
+          <div class="form-header mt-5 d-flex text-center justify-content-center">
+            Edit Child Information
+          </div>
+          <div class="add-child-form">
+            <form @submit.prevent="submitEditForm">
+              <div class="form-subtitle">
+                Child’s Name
+              </div>
+              <div class="row">
+                <div class="col mr-2">
+                  <div class="form-item-title mt-3 mb-1">
+                    First Name<span class="required-mark">*</span>
+                  </div>
+                  <input
+                    id="input1"
+                    v-model="textEditInput1"
+                    placeholder="e.g. John"
+                    class="w-100 text-input"
+                    type="text"
+                  >
+                </div>
+                <div class="col ml-2">
+                  <div class="form-item-title mt-3 mb-1">
+                    Last Name<span class="required-mark">*</span>
+                  </div>
+                  <input
+                    id="input2"
+                    v-model="textEditInput2"
+                    placeholder="e.g. Miller"
+                    class="w-100 text-input"
+                    type="text"
+                  >
+                </div>
+              </div>
+              <div class="form-subtitle mt-5">
+                Choose School’s Name and Class
+              </div>
+              <div class="row">
+                <div class="col mr-2">
+                  <div class="form-item-title mt-3 mb-1">
+                    School<span class="required-mark">*</span>
+                  </div>
+                  <select
+                    id="select1"
+                    v-model="selectEditOption1"
+                    class="combo-box w-100"
+                  >
+                    <option value="option1">
+                      Ancaster High Secondary School
+                    </option>
+                  </select>
+                </div>
+                <div class="col ml-2">
+                  <div class="form-item-title mt-3 mb-1">
+                    Class<span class="required-mark">*</span>
+                  </div>
+                  <select
+                    id="select2"
+                    v-model="selectEditOption2"
+                    class="combo-box w-100"
+                  >
+                    <option value="optionA">
+                      Grade A-10
+                    </option>
+                  </select>
+                </div>
+              </div>
+            </form>
+          </div>
+          <div class="d-flex justify-content-center my-5">
+            <div class="row align-items-center">
+              <div class="col text-center">
+                <a
+                  href="#"
+                  class="white-link"
+                  data-bs-dismiss="modal"
+                  aria-label="Close"
+                >Cancel</a>
+              </div>
+              <div class="col text-center">
+                <a
+                  href="#"
+                  type="submit"
+                  class="orange-link"
+                >Save</a>
+              </div>
+              <div class="col text-center">
+                <a
+                  href="#"
+                  class="delete-child"
+                >Delete this child</a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
   <div class="mt-5">
     <div class="child-item py-5 row d-flex">
       <div class="col-4 child-name">
@@ -130,6 +240,8 @@
           <a
             class="col"
             href="#"
+            data-bs-toggle="modal"
+            data-bs-target="#staticBackdropcEdit"
           >
             <img
               src="../../../public/images/Group.png"
@@ -159,7 +271,12 @@ export default {
       textInput1: '',
       textInput2: '',
       selectOption1: '',
-      selectOption2: ''
+      selectOption2: '',
+
+      textEditInput1: 'Courtney',
+      textEditInput2: 'Johnson',
+      selectEditOption1: 'Ancaster High Secondary School',
+      selectEditOption2: 'A-10'
     }
   },
   methods: {
@@ -170,6 +287,15 @@ export default {
         textInput2: this.textInput2,
         selectOption1: this.selectOption1,
         selectOption2: this.selectOption2
+      }
+    },
+    submitEditForm () {
+      // eslint-disable-next-line
+      const formData = {
+        textEditInput1: this.textEditInput1,
+        textEditInput2: this.textEditInput2,
+        selectEditOption1: this.selectEditOption1,
+        selectEditOption2: this.selectEditOption2
       }
     }
   }
@@ -259,10 +385,6 @@ export default {
   border: 1px solid #003046 !important;
 }
 
-.mt-6 {
-  margin-top: 6rem;
-}
-
 .child-name {
   color: #003046 !important;
   font-weight: 600 !important;
@@ -277,5 +399,11 @@ export default {
 
 .child-item {
   border-bottom: 1px solid #DADADA;
+}
+
+.delete-child {
+  font-size: 17px;
+  font-weight: 400;
+  color: #AF0C0C;
 }
 </style>

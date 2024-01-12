2f209c3701dff8c60ffe62904f0891d18a7e5f47
ivanLFN <LFN.current@yandex.com>
2024-01-11 18:42:36 -0800

    adding: user vote page

--- src/router.js
+++ src/router.js
@@ -1,10 +1,12 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomePage from './components/HomePage.vue'
 import AccountPage from './views/AccountPage.vue'
+import UserVotePage from './views/UserVotePage.vue'
 
 const routes = [
   { path: '/', component: HomePage },
-  { path: '/account', component: AccountPage }
+  { path: '/account', component: AccountPage },
+  { path: '/account/user-votes', component: UserVotePage }
 ]
 
 const router = createRouter({

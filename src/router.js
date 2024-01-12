7c6a4b5a236d53413dca2eb14561827c10759fb3
ivanLFN <LFN.current@yandex.com>
2024-01-12 12:32:52 -0800

    adding: restarants page header (search and back btn)

--- src/router.js
+++ src/router.js
@@ -2,11 +2,13 @@ import { createRouter, createWebHistory } from 'vue-router'
 import HomePage from './components/HomePage.vue'
 import AccountPage from './views/AccountPage.vue'
 import UserVotePage from './views/UserVotePage.vue'
+import RestarantsPage from './views/RestarantsPage.vue'
 
 const routes = [
   { path: '/', component: HomePage },
   { path: '/account', component: AccountPage },
-  { path: '/account/user-votes', component: UserVotePage }
+  { path: '/account/user-votes', component: UserVotePage },
+  { path: '/restarants/list', component: RestarantsPage }
 ]
 
 const router = createRouter({
---
+++ src/views/RestarantsPage.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="app-container">
+    <MainMenu class="main-menu" />
+    <div class="container mt-5">
+      <RestarantsHeader />
+      <!-- <RestarantsList /> -->
+    </div>
+    <FooterComponent class="footer-component" />
+  </div>
+</template>
+
+<script>
+import MainMenu from '@/components/MainMenu.vue'
+import FooterComponent from '@/components/FooterComponent.vue'
+import RestarantsHeader from '../components/restarants_components/RestarantsHeader.vue'
+// import RestarantsList from '../components/restarants_components/RestarantsList.vue'
+
+export default {
+  name: 'RestarantsPage',
+  components: {
+    MainMenu,
+    FooterComponent,
+    RestarantsHeader
+    // RestarantsList
+  }
+}
+</script>
+
+<style>
+</style>
+
+<style src="../../public/styles.css"></style>

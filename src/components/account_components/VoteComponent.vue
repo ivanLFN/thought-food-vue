180393e81e4e82741c0da3247dcfb9df0bb5a912
ivanLFN <LFN.current@yandex.com>
2024-01-13 12:09:10 -0800

    update: vote component

--- src/components/account_components/VoteComponent.vue
+++ src/components/account_components/VoteComponent.vue
@@ -1,8 +1,13 @@
 <template>
   <div class="row d-flex mt-5">
-    <div class="offset-1 col-6 pl-5 text">
-      Currently you haven’t vote any restaurants yet.<br>
-      Your votes will be shown at this page.
+    <div class="offset-1 col-6 pl-5">
+      <div
+        v-if="!AnyItems"
+        class="text"
+      >
+        Currently you haven’t vote any restaurants yet.<br>
+        Your votes will be shown at this page.
+      </div>
     </div>
     <div class="col-5">
       <a
@@ -13,12 +18,63 @@
       </a>
     </div>
   </div>
+
+  <div
+    v-if="AnyItems"
+    class=" mt-4"
+  >
+    <div class="header-voted">
+      Restaurants I voted for
+    </div>
+    <div class="row mt-3">
+      <div class="col-4">
+        <img
+          class="w-100 mb-3"
+          src="../../../public/images/restarant_prew_image.png"
+          alt="restarant"
+        >
+        <div class="row voted-restarants">
+          <div class="col">
+            <a
+              href="#"
+              class="restarant-link"
+            >
+              Bob’s Burgers
+            </a>
+          </div>
+          <div class="col">
+            <div class="d-flex justify-content-end">
+              <span class="my-votes">My Votes:</span>
+              2
+            </div>
+          </div>
+        </div>
+        <div class="d-flex tags py-2">
+          <div class="tag">
+            <div class="tag-name">
+              Burgers
+            </div>
+          </div>
+          <div class="tag">
+            <div class="tag-name">
+              Salad
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
 
 export default {
-  name: 'VoteComponent'
+  name: 'VoteComponent',
+  data () {
+    return {
+      AnyItems: true
+    }
+  }
 }
 </script>
 
@@ -39,5 +95,42 @@ export default {
   border-radius: 30px;
   background-color:#003046;
   text-decoration: none;
+}
+
+.header-voted {
+  color: #003046;
+  font-weight: 500;
+  font-size: 32px;
+}
+
+.my-votes {
+  margin-right: 3px;
+  font-weight: 250;
+}
+
+.voted-restarants {
+  font-weight: 550;
+  font-size: 17px;
+  color: #003046;
+  text-decoration: none;
+}
+
+.restarant-link {
+  text-decoration: none;
+  color: #003046;
+}
+
+.tag {
+  text-align: center;
+  border: 1px solid #003046;
+  border-radius: 30px;
+  margin-right: 10px;
+  font-weight: 350;
+  font-size: 15px;
+  color: #003046;
+}
+
+.tag-name {
+  padding: 3px 20px;
 }
 </style>

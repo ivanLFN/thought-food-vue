3600e25d2485a2f43c6415a8c4534bac14cf3f9b
ivanLFN <LFN.current@yandex.com>
2024-01-12 20:30:28 -0800

    adding: RestarantDescription component

---
+++ src/views/RestarantPage.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="app-container">
+    <MainMenu class="main-menu" />
+    <div class="container mt-5">
+      <RestarantDescription />
+    </div>
+    <FooterComponent class="footer-component" />
+  </div>
+</template>
+
+<script>
+import MainMenu from '@/components/MainMenu.vue'
+import FooterComponent from '@/components/FooterComponent.vue'
+import RestarantDescription from '../components/restarants_components/RestarantDescription.vue'
+
+export default {
+  name: 'RestarantsPage',
+  components: {
+    MainMenu,
+    FooterComponent,
+    RestarantDescription
+  }
+}
+</script>
+
+<style>
+</style>
+
+<style src="../../public/styles.css"></style>

2d40390a12f6d117e5034c4516f9c7a39ea49e2b
ivanLFN <LFN.current@yandex.com>
2024-01-17 00:09:47 -0800

    adding: login and Signup pages with forms

---
+++ src/views/SignUpPage.vue
@@ -0,0 +1,50 @@
+<template>
+  <div class="app-container">
+    <div class="container">
+      <div class="mt-5">
+        <a
+          class="signup-header"
+          href="#"
+        >
+          Food For Thought
+        </a>
+      </div>
+    </div>
+    <div class="container signup-el">
+      <div class="row">
+        <div class="offset-4 col-md-3 col-12">
+          <SignUpForm />
+        </div>
+      </div>
+    </div>
+    <FooterComponent class="footer-component" />
+  </div>
+</template>
+
+<script>
+import SignUpForm from '@/components/account_components/SignUpForm.vue'
+import FooterComponent from '@/components/FooterComponent.vue'
+export default {
+  name: 'SignUpPage',
+  components: {
+    SignUpForm,
+    FooterComponent
+  }
+}
+</script>
+
+<style scoped>
+.signup-header {
+  font-size: 30px !important;
+  font-weight: 700;
+  color: #003046 !important;
+  line-height: 38px;
+  text-decoration: none;
+}
+
+.signup-el {
+  margin-top: 20%;
+}
+</style>
+
+<style src="../../public/styles.css"></style>
